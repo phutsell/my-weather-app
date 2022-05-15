b179ea4a1df63343d3b080b44c5033d441d4231c
phutsell <105092944+phutsell@users.noreply.github.com>
2022-05-15 09:54:54 -0700

    Added Alt text

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ function showTemperature(response) {
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 function submitCity(event) {
